98d4199d81e3984114227eae89036f3d8d02ff3e
Eoin Halligan <eoin@losant.com>
2022-12-06 11:16:44 -0500

    day 6 solutions

--- dec-6-part-two.js
+++ dec-6-part-two.js
@@ -3,22 +3,15 @@ import readline from 'readline';
 
 const input = fs.createReadStream('./files/dec-6.txt', 'utf-8');
 const reader = readline.createInterface({input});
-
-const buffer = [];
-reader.on('line', line => buffer.push(line));
-
-reader.on('close', () => {
-    for(let i = 0; i < buffer.length; i++) {
-        const current = buffer[i];
-        const charSet = [...buffer[i]];
-        const bufferSet = [];
-        for(let j = 0; j < charSet.length; j++) {
-            const str = current.slice(j, j + 14);
-            bufferSet.push([...new Set(str)])
-        }
-        const match = bufferSet.find(x => x.length === 14);
-        const str = match.reduce((acc, val) => acc.concat(val),'')
-        const chars = current.indexOf(str) + str.length;
-        console.log(chars) // 2508
+const offset = 14;
+reader.on('line', line => {
+    const bufferSet = [];
+    for(let i = 0; i < [...line].length; i++) {
+        const str = line.slice(i, i + offset);
+        bufferSet.push([...new Set(str)])
     }
-})
+    const matchStr = bufferSet.find(x => x.length === offset).reduce((acc, val) => acc.concat(val),'');
+    const chars = line.indexOf(matchStr) + matchStr.length;
+    console.log(chars) // 2508
+});
+
