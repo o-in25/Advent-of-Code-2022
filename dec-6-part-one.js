4425baef769027f14821bd9535175da2c025ed60
Eoin Halligan <eoin@losant.com>
2022-12-06 11:10:55 -0500

    day 6 solutions

---
+++ dec-6-part-one.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const input = fs.createReadStream('./files/dec-6.txt', 'utf-8');
+const reader = readline.createInterface({input});
+
+const buffer = [];
+reader.on('line', line => buffer.push(line));
+
+reader.on('close', () => {
+    for(let i = 0; i < buffer.length; i++) {
+        const current = buffer[i];
+        const charSet = [...buffer[i]];
+        const bufferSet = [];
+        for(let j = 0; j < charSet.length; j++) {
+            const str = current.slice(j, j + 4);
+            bufferSet.push([...new Set(str)])
+        }
+        const match = bufferSet.find(x => x.length === 4);
+        const str = match.reduce((acc, val) => acc.concat(val),'')
+        const chars = current.indexOf(str) + str.length;
+        console.log(chars) // 1804
+    }
+})
