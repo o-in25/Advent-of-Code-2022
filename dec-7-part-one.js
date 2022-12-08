7cf206e986e77e200589c029ddda518b7a735833
o-in25 <ehalligan12@gmail.com>
2022-12-08 16:49:44 -0500

    day 1 working

--- dec-7-part-one.js
+++ dec-7-part-one.js
@@ -6,95 +6,107 @@ const input = fs.createReadStream('./files/dec-7.txt', 'utf-8');
 const reader = readline.createInterface({input});
 
 
-
-const tree = {
-    name: '/',
-    children: [],
-    size: 0
-}
-
-let current = tree;
-reader.on('line', line => {
-    if(line === '$ cd e') {
-        console.log(line)
-    }
-    switch(line) {
-
-
-        // cd /
-        case line.match(/\$ cd (\/)/)?.input: {
-            current = tree;
-            break;
-        }
-
-        // cd ..
-        case line.match(/\$ cd (\.\.)/)?.input: {
-            current = {...current.parent};
-            break;
-        }
-
-        // cd <dir>
-        case line.match(/\$ cd [a-z]+/)?.input: {
-            const name = line.split(' ')[2];
-            current = current.children.find(x => x.name === name);
-            break;
-
-        }
-
-        // ls
-        case line.match(/\$ ls/)?.input: {
-            break;
-        }
-
-        // dir <name>
-        case line.match(/dir [a-z]+/)?.input: {
-            const name = line.split(' ')[1];
-            const node = {
-                name,
-                parent: current,
-                children: [],
-                size: 0
-            }
-            current.children.push(node)
-            break;
-        }
-
-        // files
-        default: {
-            const size = line.split(' ')[0];
-            current.size += Number(size);
-            break;   
-        }
-    }
-
-});
-
-function getSize(node) {
-    if(node.children.length > 0) {
-        for(const child of node.children) {
-            node.size += child.size;
-            if(node.parent) {
-                 node.parent.size += child.size;
-            }
-            getSize(child);
-        }
-    }
-}
-
+const lines = [];
+reader.on('line', line => lines.push(line));
 reader.on('close', () => {
-    getSize(tree);
-    let sum = 0;
-    function filterTree(node, threshold) {
-        if (node.children.length) {
-            for (const child of node.children) {
-                if(child.size < threshold) {
-                    sum += child.size
-                }
-                filterTree(child, threshold);
-            }
-        }
-    }    
-    filterTree(tree, 100000)
-    console.log(sum);
+    const tree = buildTree(lines);
+    showTree(tree);
+
+    const threshold = 100000;
+    let runningSum = 0;
+
+    find(tree, (node, size) => {
+      if(size <= threshold) {
+        runningSum += size;
+      }
+    });
+  
+    console.log(runningSum);
 });
 
+function find(node, callback = () => {}) {
+    if(node.type !== 'directory') {
+        return Number(node.size);
+    }
+
+    const sizes = node.children.map(child => find(child, callback));
+    const sum = sizes.reduce((acc, val) => acc + val, 0)
+    callback(node, sum);
+    return sum;
+}
+
+function showTree(node, depth = 0) {
+    console.log(`${' '.repeat(depth * 2)} - ${node.name} ${node.type === 'directory'? '(dir)' : `file, size=${node.size})`}`);
+    for(let child of node.children) {
+        showTree(child, depth + 1);
+    }
+}
+
+function buildTree(lines) {
+    const tree = {
+        name: '/',
+        children: [],
+        size: 0,
+        type: 'directory'
+    }
+    let current = tree;
+    lines.forEach(line => {
+        switch(line) {
+
+            // cd /
+            case line.match(/\$ cd (\/)/)?.input: {
+                current = tree;
+                break;
+            }
+    
+            // cd ..
+            case line.match(/\$ cd (\.\.)/)?.input: {
+                current = {...current.parent};
+                break;
+            }
+    
+            // cd <dir>
+            case line.match(/\$ cd [a-z]+/)?.input: {
+                const name = line.split(' ')[2];
+                current = current.children.find(x => x.name === name);
+                break;
+    
+            }
+    
+            // ls
+            case line.match(/\$ ls/)?.input: {
+                break;
+            }
+    
+            // dir <name>
+            case line.match(/dir [a-z]+/)?.input: {
+                const name = line.split(' ')[1];
+                const node = {
+                    name,
+                    size: 0,
+                    parent: current,
+                    children: [],
+                    type: 'directory'
+                }
+                current.children.push(node)
+                break;
+            }
+    
+            // files
+            default: {
+                const [size, name] = line.split(' ');
+                const node = {
+                    name, 
+                    size,
+                    parent: current,
+                    children: [],
+                    type: 'file'
+                }
+                current.children.push(node)
+                break;
+            }
+        }
+    });
+
+    return tree;
+}
