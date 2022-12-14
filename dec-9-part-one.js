b4d588408aabb139433e8156afa1fe1d66e57ac6
o-in25 <ehalligan12@gmail.com>
2022-12-13 19:57:33 -0500

    day noine solution

--- dec-9-part-one.js
+++ dec-9-part-one.js
@@ -7,39 +7,65 @@ const lines = readFileSync("./files/dec-9.txt", { encoding: "utf-8" }).replace(/
         const [letter, number] = line.split(' ');
         const distance = Number(number);
         const direction = letter.toLowerCase();
-        return {direction, distance};
+        return { direction, distance };
     });
 
-
 class Knot {    
-    // private
-    static #moves = {
+    static moves = {
         r: {x: 1, y: 0},
         l: {x: -1, y: 0},
         u: {x: 0, y: 1},
         d: {x: 0, y: -1}
     };
-    // private
-    #visted = new Set();
-    get visted() {
-        return this.#visted;
+
+    // https://en.wikipedia.org/wiki/Taxicab_geometry
+    static manhattanDistance(x1, x2, y1, y2) {
+        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
     }
+
+    static isDiagonal(dx, dy) {
+        return Math.abs(dx) - Math.abs(dy) === 0;
+    }
+
+    visted = new Set();
 
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
-    move(nextMove) {
-        const { direction } = nextMove;
-        const dx = Knot.#moves[direction].x;
-        const dy = Knot.#moves[direction].y;
-        this.x += dx;
-        this.y += dy;
+
+    move(direction) {
+        const move = Knot.moves[direction];
+        const {x, y} = move;
+        this.x += x;
+        this.y += y;
     }
 
-    markVisited(move) {
-        this.#visted.add(hash(move));
+    follow(knot) {
+        const distance = Knot.manhattanDistance(knot.x, this.x, knot.y, this.y);
+        const dx = knot.x - this.x;
+        const dy = knot.y - this.y;
+        // distance is 2 or more and we're not diagonal
+        if(distance >= 2 && !Knot.isDiagonal(dx, dy)) {
+            // we're not touching
+            if(dx > 0) {
+                this.x++;
+            } else if(dx < 0) {
+                this.x--;
+            }
+
+            if(dy > 0) {
+                this.y++;
+            } else if(dy < 0) {
+                this.y--;
+            }
+        
+        }
+    }
+
+    markVisited(x, y) {
+        this.visted.add(hash({x, y}));
     }
 }
 
@@ -47,8 +73,14 @@ const head = new Knot(0, 0);
 const tail = new Knot(0, 0);
 
 lines.forEach(line => {
-    head.move(line);
+    const {direction, distance} = line;
+    for(let i = 0; i < distance; i++) {
+        head.move(direction);
+        tail.follow(head);
+        tail.markVisited(tail.x, tail.y);
+    }
 })
 
-console.log(head)
+const size = tail.visted.size;
+console.log(size) // 6470
 
