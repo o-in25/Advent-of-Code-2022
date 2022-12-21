4a70db59da0ab4027818b19e3c712a3f98ef3ad0
o-in25 <ehalligan12@gmail.com>
2022-12-21 18:26:46 -0500

    working day 12

--- dec-12-part-two.js
+++ dec-12-part-two.js
@@ -13,26 +13,39 @@ const grid = readFileSync("./files/dec-12.txt", { encoding: "utf-8" })
     .split("\n")
     .map(line => [...line]);
     
-const [start, end] = (grid => {
+const {points, end} = (grid => {
+    let end;
     const points = [];
     for(let row = 0; row < grid.length; row++) {
         for(let col = 0; col < grid[row].length; col++) {
             const current = grid[row][col];
-            if(current === 'S' || current === 'E') {
+            if(current === 'S') {
+                grid[row][col] = 'a';
+                points.push(new Node(row, col));
+            } else if(current === 'E') {
+                grid[row][col] = 'z';
+                end = new Node(row, col);
+            } else if(current === 'a') {
                 points.push(new Node(row, col));
             }
         }
     }
-    return points;
+    return {points, end};
 })(grid);
 
-grid[end.row][end.col] = 'z'
-grid[start.row][start.col] = 'a'
 const CHAR_OFFSET = 96;
-const visted = new Set();
+
+// get bfs for each point
+const sums = points.reduce((acc, curr) => {
+    const visted = new Set();
+    const distance = bfs(curr, end, visted);
+    acc.push(distance);
+    return acc;
+}, []).sort((a, b) => a - b)
+console.log(sums[0]) // 321
 
 
-function bfs(start, end) {
+function bfs(start, end, visted) {
     const queue = [];
     queue.push({node: start, distance: 0});
     visted.add(`(${start.row}, ${start.col})`)
@@ -47,27 +60,27 @@ function bfs(start, end) {
         }
 
         // check left
-        if(isSafe(node, node.row - 1, node.col) && !hasVisited(node.row - 1, node.col)) {
+        if(isSafe(node, node.row - 1, node.col) && !hasVisited(node.row - 1, node.col, visted)) {
             const newNode = enqueue(queue, node.row - 1, node.col, distance);
-            markVisted(newNode);
+            markVisted(newNode, visted);
         }
 
         // check down 
-        if(isSafe(node, node.row, node.col - 1) && !hasVisited(node.row, node.col - 1)) {
+        if(isSafe(node, node.row, node.col - 1) && !hasVisited(node.row, node.col - 1, visted)) {
             const newNode = enqueue(queue, node.row, node.col - 1, distance);
-            markVisted(newNode);
+            markVisted(newNode, visted);
         }
 
         // check down 
-        if(isSafe(node, node.row, node.col + 1) && !hasVisited(node.row, node.col + 1)) {
+        if(isSafe(node, node.row, node.col + 1) && !hasVisited(node.row, node.col + 1, visted)) {
             const newNode = enqueue(queue, node.row, node.col + 1, distance);
-            markVisted(newNode);
+            markVisted(newNode, visted);
         }
 
         // check down 
-        if(isSafe(node, node.row + 1, node.col) && !hasVisited(node.row + 1, node.col)) {
+        if(isSafe(node, node.row + 1, node.col) && !hasVisited(node.row + 1, node.col, visted)) {
             const newNode = enqueue(queue, node.row + 1, node.col, distance);
-            markVisted(newNode);
+            markVisted(newNode, visted);
         }
     }
 }
@@ -89,13 +102,11 @@ function isSafe(current, newRow, newCol) {
     return newChar - currentChar <= 1;
 }
 
-function hasVisited(newRow, newCol) {
+function hasVisited(newRow, newCol, visted) {
     return visted.has(`(${newRow}, ${newCol})`);
 }
 
-function markVisted(node) {
+function markVisted(node, visted) {
     visted.add(`(${node.row}, ${node.col})`)
 }
 
-const distance = bfs(start, end);
-console.log(distance) // 330
