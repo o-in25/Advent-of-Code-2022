8e3bc5cb8279a8cfa8c76fe015d7d9189d54c9b6
o-in25 <ehalligan12@gmail.com>
2022-12-23 15:42:06 -0500

    niave solution for dec-13

--- dec-13/part-one.js
+++ dec-13/part-one.js
@@ -1,4 +1,4 @@
-import { readFileSync } from "fs";
+import { lstat, readFileSync } from "fs";
 import _ from "lodash";
 const pairs = _.chunk(readFileSync("./files/dec-13.txt", { encoding: "utf-8" })
     .replace(/\r/g, "")
@@ -6,24 +6,68 @@ const pairs = _.chunk(readFileSync("./files/dec-13.txt", { encoding: "utf-8" })
     .filter(packet => packet !== '')
     .map(packet => JSON.parse(packet)), 2);
 
-// pairs.forEach(pair => {
-//     const [left, right] = pair;
-//     if(left.length === 0) {
+const correctPairs = [];
+const incorrectPairs = [];
 
+let pairNumber = 1;
+for(const pair of pairs) {
+    const [left, right] = Object.assign([], pair);
+    const valid = read(left, right)
+    console.log(`${valid} == Pair ${pairNumber} ==`)
+    pairNumber++;
+}
 
-//     } else if(2) {
+function read(left, right) {
+    const lhs = left.shift();
+    const rhs = right.shift();
 
-//     }
-
-// });
-
-function emptyList(list) {
-    if(Array.isArray(list) && list.length !== 0) {
-        return emptyList(list.shift())
+    if(!lhs) {
+        return true;
     }
-    return Array.isArray(list) && list.length === 0;
+
+    if(!rhs) {
+        return false;
+    }
+
+    // both numbers
+    if(!Array.isArray(lhs) && !Array.isArray(rhs)) {
+        if(lhs !== rhs) {
+            return lhs < rhs;
+        }
+    } else if(Array.isArray(lhs) && Array.isArray(rhs)) {
+        return read(lhs, rhs);
+    } else {
+        // left one array? then right is number
+        if(Array.isArray(lhs)) {
+            return read(lhs, [rhs])
+        } else {
+            // probs dont need to do else here but fuck it
+            return read([lhs], rhs)
+        }
+    }
+
+
+    if(!left.length && !right.length) {
+        return true;
+    }
+    return read(left, right);
+    // 1 of each
+
+
 
 }
 
-const res = emptyList([[[15]]]);
-console.log(res)
+
+function isListEmpty(list) {
+    if(Array.isArray(list)) {
+        if(list.length === 0) {
+            return true;
+        }
+        return isListEmpty(list.shift());
+    }
+
+    return false;
+
+}
+
+
